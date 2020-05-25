ecb7c29420b455518a754ef7799b6430b3da5d0e
zhongqiaohua <953901509@qq.com>
2020-05-25 09:08:36 +0800

    添加了BranchesController

--- demo05/src/main/java/com/example/demo05/controller/HelloController.java
+++ demo05/src/main/java/com/example/demo05/controller/HelloController.java
@@ -3,11 +3,6 @@ package com.example.demo05.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Arrays;
-import java.util.Map;
 
 @Controller
 public class HelloController {
---
+++ demo05/src/main/java/com/example/demo05/controller/TestBranchesController.java
@@ -0,0 +1,7 @@
+package com.example.demo05.controller;
+
+public class TestBranchesController {
+    public static void main(String[] args) {
+        System.out.println("Branches test~~~");
+    }
+}
